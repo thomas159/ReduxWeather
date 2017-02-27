67ed0b02b1a8e6d4c78e87369d671545257e4e1a
thomas159 <thomas159@gmail.com>
2017-02-27 15:28:41 +0100

    complete project

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,10 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
-	const url = `${ROOT_URL}&q={city},us`;
+	const url = `${ROOT_URL}&q=${city},uk`;
 	const request = axios.get(url);
 
-	// console.log('Request:', request);
+	console.log('Request:', request);
 	
 	return {
 		type: FETCH_WEATHER,
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import Chart from "../components/chart";
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
+	// render single city
 	renderWeather(cityData) {
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humiditys = cityData.list.map(weather => weather.main.humidity);
+		const { lon,lat } = cityData.city.coord;
+		console.log(cityData.city.coord);
+
+
 		return (
-			<tr>
-				<td>{ cityData.city.name }</td>
+			<tr key={cityData.city.id}>
+				<td><GoogleMap lon={lon} lat={lat} /></td>
+				<td>
+					<Chart data={temps} color="red" units="K" />
+				</td>
+				<td>
+					<Chart data={pressures} color="orange" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humiditys} color="green" units="%" />
+				</td>
 			</tr>
 		);
 	}
@@ -18,9 +36,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temp</th>
-						<th>Pressure</th>
-						<th>Humidty</th>
+						<th>Temp (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidty (%)</th>
 					</tr>
 				</thead>
 					<tbody>
